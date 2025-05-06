08e0fca430e5f40406b3d8323d888896988b2958
Gabriel Eudes <gaeudes@student.42.fr>
2025-05-06 16:31:08 +0200

    Disk storage

--- monitoring.sh
+++ monitoring.sh
@@ -4,6 +4,5 @@ Archi=$(uname -a)
 pCPU=$(lscpu -p | grep -v "#" | wc -l)
 vCPU=$(echo "$pCPU * $(lscpu | grep 'Thread' | awk '{print $4}') * $(lscpu | grep 'Core(s)' | awk '{print $4}')" | bc)
 Mem=$(free -m | grep Mem | awk '{print $3"/"$2}')
-Total=$(free -tm | grep Total | awk '{print $3"/"$2}')
-
-
+TotalMem=$(free -tm | grep Total | awk '{print $3"/"$2}')
+DiskUsage=$(df -h / | grep -v Filesystem | awk '{sub(/G$/, "", $3); print $3"/"$2"  ("$5")"}')
