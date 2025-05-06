6726b9e9d6b04ba8ddca7aa4fdca17be0d84efca
Gabriel Eudes <gaeudes@student.42.fr>
2025-05-06 16:10:04 +0200

    Mem parsed

--- monitoring.sh
+++ monitoring.sh
@@ -3,5 +3,7 @@
 Archi=$(uname -a)
 pCPU=$(lscpu -p | grep -v "#" | wc -l)
 vCPU=$(echo "$pCPU * $(lscpu | grep 'Thread' | awk '{print $4}') * $(lscpu | grep 'Core(s)' | awk '{print $4}')" | bc)
+Mem=$(free -m | grep Mem | awk '{print $3"/"$2}')
+Total=$(free -tm | grep Total | awk '{print $3"/"$2}')
 
-echo "$vCPU"
+
