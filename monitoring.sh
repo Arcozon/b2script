ea49347f1b0756a015bb331f798adba96c010d4c
Gabriel Eudes <gaeudes@student.42.fr>
2025-05-06 15:13:04 +0200

    cpu done

--- monitoring.sh
+++ monitoring.sh
@@ -2,6 +2,6 @@
 
 Archi=$(uname -a)
 pCPU=$(lscpu -p | grep -v "#" | wc -l)
-vCPU=$(echo "$pCPU * $(lscpu | grep 'Thread' | awk '{print $4}') * $(lscpu | grep 'Core(s)' | awk '{print $4}')")
+vCPU=$(echo "$pCPU * $(lscpu | grep 'Thread' | awk '{print $4}') * $(lscpu | grep 'Core(s)' | awk '{print $4}')" |xargs  bc)
 
 echo "$vCPU"
