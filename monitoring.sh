fd567036254e45012ee9771ab45aa3beed81c8f5
Gabriel Eudes <gaeudes@student.42.fr>
2025-05-07 15:02:10 +0200

    meme space

--- monitoring.sh
+++ monitoring.sh
@@ -4,8 +4,8 @@ Archi=$(uname -a)
 pCPU=$(lscpu -p | grep -vc "#"l)
 vCPU=$(echo "$pCPU * $(lscpu | grep 'Thread' | awk '{print $4}') * $(lscpu | grep 'Core(s)' | awk '{print $4}')" | bc)
 
-Mem=$(free -m | grep Mem | awk '{printf "%s/%s (%.1f%%)", $3, $2, $3/$2*100}')
-TotalMeme=$(free -tm | grep Total | awk '{printf "%s/%s (%.1f%%)", $3, $2, $3/$2*100}')
+Mem=$(free -m | grep Mem | awk '{printf "%s/%s  (%.1f%%)", $3, $2, $3/$2*100}')
+TotalMeme=$(free -tm | grep Total | awk '{printf "%s/%s  (%.1f%%)", $3, $2, $3/$2*100}')
 
 txt_DiskUsage=$(df -h / | grep -v Filesystem | awk '{sub(/G$/, "", $3); print $3"/"$2"  ("$5")"}')
 txt_CPUUsage=$(top -b -n 1 | grep Cpu | awk -F',' '{ print $4}' | awk -F" " '{print $1}' | awk '{printf("%.1f%%\n", 100-$1)}')
